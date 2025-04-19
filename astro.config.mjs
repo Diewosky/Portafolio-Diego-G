f5e5be39b3a692956eee11f041f77331d8edaf31
Diewosky <diego.gutierrez2001@alumnos.ubiobio.cl>
2025-04-19 18:50:55 -0400

    Fix: Corregido problema de rutas para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,8 @@ export default defineConfig({
   integrations: [tailwind()],
   site: 'https://diewosky.github.io',
   base: '/Portafolio-Diego-G',
+  trailingSlash: 'always',
+  build: {
+    format: 'directory',
+  }
 });
